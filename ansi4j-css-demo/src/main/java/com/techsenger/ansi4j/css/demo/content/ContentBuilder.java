90a7dd15968a564203720a82bf6e5853506f89c6
Pavel Castornii <pavelcastornii@gmail.com>
2024-11-30 17:06:21 +0200

    Update license

--- ansi4j-css-demo/src/main/java/com/techsenger/ansi4j/css/demo/content/ContentBuilder.java
+++ ansi4j-css-demo/src/main/java/com/techsenger/ansi4j/css/demo/content/ContentBuilder.java
@@ -1,8 +1,17 @@
 /*
- * Copyright 2022-2024 Pavel Castornii.
+ * Copyright 2024 Pavel Castornii.
  *
- * This project is dual-licensed under the GNU AGPL version 3 and a commercial license.
- * See the file LICENSE.md in the root directory of the project for full license information.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.techsenger.ansi4j.css.demo.content;
